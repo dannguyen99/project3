c677dc8c084218d084ca5877aea52108f65e6f44
dannguyen <dannguyen0801@gmail.com>
2019-05-10 13:04:45 +0700

    added pizza folder

--- pizza/urls.py
+++ pizza/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth import views as auth_views
-from users.views import register
 
 urlpatterns = [
     path("", include("orders.urls")),
     path("admin/", admin.site.urls),
-	path('register/', register, name='register'),
-	path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),	
 ]
