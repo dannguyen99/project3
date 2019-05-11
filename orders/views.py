984bd13229b6b4cd996de962f0b6c0076cdfc283
dannugyen <dannguyen0801@gmail.com>
2019-05-11 16:34:12 +0700

    -added home url

--- orders/views.py
+++ orders/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Project 3: TODO")
+
+def home(request):
+    return render(request, "orders/home.html")
