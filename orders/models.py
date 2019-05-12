92a2e424d22d224b55ecefaebfe64becf78a36a2
dannugyen <dannguyen0801@gmail.com>
2019-05-12 11:37:59 +0700

    added pizza to models.py

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.model):
+    size = models.CharField(max_length=5)
+    type = models.CharField(max_length=20)
+    price = models.DecimalField(decimal_places=2)
