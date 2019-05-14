99bf475e29388c6f64d7b4d016f6f1f10956205a
dannugyen <dannguyen0801@gmail.com>
2019-05-14 14:03:50 +0700

    added models to models.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Pizza, Topping, Sub, Pasta, Salad
 # Register your models here.
+admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(Sub)
+admin.site.register(Pasta)
+admin.site.register(Salad)
--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 
 # Create your models here.
-class Pizza(models.model):
-    size = models.CharField(max_length=5)
-    type = models.CharField(max_length=20)
-    price = models.DecimalField(decimal_places=2)
+class Pizza(models.Model):
+    Regular = 'RL'
+    Sicilian = 'SC'
+    type_choices = ((Regular,'Regular'), (Sicilian, 'Sicilian'))
+    name = models.CharField(max_length=64, default="Italy")
+    type = models.CharField(max_length=20, choices = type_choices)
+    price_small = models.DecimalField(max_digits=5, decimal_places=2)
+    price_large = models.DecimalField(max_digits=5, decimal_places=2,default = 0)
+    def __str__(self):
+        return f"{self.name} - {self.type} - {self.price_small} - {self.price_large}"
+
+class Topping(models.Model):
+    name = models.CharField(max_length = 64)
+
+class Sub(models.Model):
+    name = models.CharField(max_length = 64)
+
+class Pasta(models.Model):
+    name = models.CharField(max_length = 64)
+
+class Salad(models.Model):
+    name = models.CharField(max_length = 64)
+
+class Dinner_Platter(models.Model):
+    name = models.CharField(max_length = 64)
